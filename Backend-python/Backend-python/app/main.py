b204a58636a4dd57f79b28f49e86b3355ce43a0b
Jehan Meqdad <116122834+jehanmeqdad@users.noreply.github.com>
2026-04-26 22:58:44 +0300

    Update main.py

--- Backend-python/Backend-python/app/main.py
+++ Backend-python/Backend-python/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.analysis_routes import router as analysis_router
 from app.api.station_routes import router as station_router
+from app.api.loyalty_routes import router as loyalty_router
+from app.api.offer_routes import router as offer_router
 
 app = FastAPI(title="PetroVision API")
 
@@ -15,6 +17,8 @@ app.add_middleware(
 
 app.include_router(analysis_router)
 app.include_router(station_router)
+app.include_router(loyalty_router)
+app.include_router(offer_router)
 
 @app.get("/")
 def root():
